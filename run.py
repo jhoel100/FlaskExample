08775a3969154d1d26b38c7221a212381864db89
senabi <sensenabi@gmail.com>
2021-07-08 08:37:59 -0500

    base

--- run.py
+++ run.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS, cross_origin
 import random
 from datetime import datetime
 import csv
 from numpy.core.numeric import moveaxis
 import pandas as pd
 import numpy as np
+import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 titanic_df = pd.read_csv("static/data/train.csv")
 survived = titanic_df[(titanic_df['Survived'] == 1)
                       & (titanic_df["Age"].notnull())]
-
 
 crimes_df = pd.read_csv("static/data/Crimes.csv")
 crimes_sel = crimes_df[(crimes_df["X Coordinate"].notnull())
@@ -64,6 +67,7 @@ def calculate_percentage(val, total):
 
 
 @app.route('/api/coords/crime_type')
+@cross_origin()
 def get_coords_by_crime_type():
     grouped = crimes_sel.groupby("Primary Type")
     # print(np.array(grouped))
@@ -89,11 +93,13 @@ def get_coords_by_crime_type():
 
 
 @app.route('/get_piechart_data')
+@cross_origin()
 def get_piechart_data():
     class_labels = crimes_df['Primary Type'].unique()
 
-    pclass_percent = calculate_percentage(crimes_sel.groupby(
-        'Primary Type').size().values, crimes_sel['ID'].count())*100
+    pclass_percent = calculate_percentage(
+        crimes_sel.groupby('Primary Type').size().values,
+        crimes_sel['ID'].count()) * 100
     pieChartData = []
     for index, item in enumerate(pclass_percent):
         eachData = {}
@@ -108,7 +114,7 @@ def get_piechart_data():
 def get_barchart_data():
     com_labels = ['2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010',
                   '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
-    
+
     com_labels   = crimes_df['Year'].unique().tolist()
     com_labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre']
     class_labels = crimes_df['Primary Type'].unique()
@@ -147,6 +153,7 @@ def get_barchart_data():
 
 
 @app.route('/api/coords/by_year')
+@cross_origin()
 def get_coords_by_year():
     grouped = crimes_sel.groupby("Year")
     class_labels = crimes_df['Year'].unique().tolist()
@@ -171,6 +178,7 @@ def get_coords_by_year():
 
 
 @app.route('/api/coords/by_month')
+@cross_origin()
 def get_coords_by_month():
     # by_year = crimes_sel.groupby("Year")
     class_labels = crimes_df['Year'].unique().tolist()
@@ -206,7 +214,6 @@ def get_piechart_data():
         pieChartData.append(eachData)
 
     return jsonify(pieChartData) """
-
 """ @app.route('/get_barchart_data')
 def get_barchart_data():
     age_labels = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79']
@@ -250,20 +257,23 @@ def get_barchart_data():
         eachBarChart['category'] = age_labels[index]
         eachBarChart['measure'] = round(item,1)
         barChartData.append(eachBarChart)
-    
+
     return jsonify(barChartData) """
 
 
 @app.route('/get_barchart_data')
+@cross_origin()
 def get_barchart_data():
-    month_labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-                    'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    month_labels = [
+        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+        'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ]
     class_labels = crimes_df['Primary Type'].unique()
 
     month_recount = {}
     for index, row in crimes_sel.iterrows():
         idate = datetime.strptime(row['Date'], '%m/%d/%Y %H:%M:%S %p')
-        month = month_labels[idate.month-1]
+        month = month_labels[idate.month - 1]
 
         if month in month_recount:
             month_recount[month] += 1
@@ -274,7 +284,7 @@ def get_barchart_data():
     for index, row in crimes_sel.iterrows():
         crime_type = row['Primary Type']
         idate = datetime.strptime(row['Date'], '%m/%d/%Y %H:%M:%S %p')
-        month = month_labels[idate.month-1]
+        month = month_labels[idate.month - 1]
         if crime_type in class_month_recount:
             if month in class_month_recount[crime_type]:
                 class_month_recount[crime_type][month] += 1
@@ -283,7 +293,6 @@ def get_barchart_data():
         else:
             class_month_recount[crime_type] = {}
             class_month_recount[crime_type][month] = 1
-
     """ Posible optimizacion al momento de calcular los recuentos """
     barChartData = []
     total = 0
@@ -294,7 +303,7 @@ def get_barchart_data():
         obj["category"] = key
         obj["group"] = "All"
 
-        obj["measure"] = round((value/total)*100, 1)
+        obj["measure"] = round((value / total) * 100, 1)
         barChartData.append(obj)
 
     for crime_type, value in class_month_recount.items():
@@ -305,10 +314,9 @@ def get_barchart_data():
             obj = {}
             obj["category"] = month
             obj["group"] = crime_type
-            obj['measure'] = round((recount/crime_total)*100, 1)
+            obj['measure'] = round((recount / crime_total) * 100, 1)
             barChartData.append(obj)
-
-    """ 
+    """
     Falta realizar validacion de que todos los tipos de crimenes
     y meses esten completos, en caso no se hayan registrado casos
     tienen que tener el valor 0.
@@ -318,11 +326,13 @@ def get_barchart_data():
 
 
 @app.route('/get_map_data')
+@cross_origin()
 def get_map_data():
     return 1
 
 
 @app.route('/get_dataset_header')
+@cross_origin()
 def get_dataset_header():
     rows = 10
     dataset_header = {}
@@ -343,8 +353,61 @@ def get_dataset_header():
 
 
 @app.route('/preanalisis')
+@cross_origin()
 def get_metadata():
     return render_template('preanalisis.html')
+
+
+@app.route('/get_columns')
+@cross_origin()
+def get_columns():
+    # print(crimes_df.head())
+    shapedf = crimes_df.shape
+    print(shapedf[0])
+    print(shapedf[1])
+    # print(crimes_df.isna().sum())
+
+    column_names = crimes_df.columns.tolist()
+
+    columns_res = []
+    # print("TYPES")
+
+    print(crimes_df.describe())
+    description = crimes_df.describe(include="all")
+
+    for c in crimes_df:
+        print(c)
+        col = dict()
+        col['name'] = c
+        col['dtype'] = str(crimes_df[c].dtypes)
+        # print("~~~TYPE")
+        # print(crimes_df[c].dtypes)
+        col['type'] = str(type(crimes_df[c].iloc[0]).__name__)
+        col['rows_count'] = str(crimes_df[c].dropna().count())
+        col['nan_count'] = str(crimes_df[c].isna().sum())
+        col['mean'] = str(description[c]['mean'])
+        # col['median'] = str(description[c]['median'])
+        col['25_percentile'] = str(description[c]['25%'])
+        col['50_percentile'] = str(description[c]['50%'])
+        col['75_percentile'] = str(description[c]['75%'])
+        col['min'] = str(description[c]['min'])
+        col['max'] = str(description[c]['max'])
+        # print(crimes_df[c].mean())
+        # col['mean'] = str(crimes_df[c].mean())
+        # if isinstance(crimes_df[c], str):
+        # print("str===========")
+        columns_res.append(col)
+
+    response = {
+        "metadata": {
+            "rows_count": shapedf[0],
+            "columns_count": shapedf[1],
+            "total_nan_rows": "xd",
+            "column_names": column_names
+        },
+        "columns": columns_res
+    }
+    return jsonify(response)
 
 
 if __name__ == '__main__':
